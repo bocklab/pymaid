1437318bc4dab9678fd61c93e0f01002d692f972
Eric Perlman <perlman@gmail.com>
2021-12-16 10:08:28 -0500

    Revert removal of vispy include.

--- pymaid/utils.py
+++ pymaid/utils.py
@@ -26,8 +26,7 @@ import numpy as np
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    #Comment to avoid "file system relative paths not allowed in hardened programs" withv vispy
-    #import vispy.visuals
+    import vispy.visuals
 
 from . import core, fetch, config, client
 
